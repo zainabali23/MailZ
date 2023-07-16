557ec67bf1692bcab666a0cb583484099f1ee8e4
perweena182 <111169795+perweena182@users.noreply.github.com>
2023-07-17 00:00:32 +0530

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,33 @@
+export const gmailLogo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Fp-2VsfYqUTIhNGm3zBK8P6HKMuKISa7zvwyZH7XFIy6sFsp0sGw28tc3SFFfRywGyU&usqp=CAU';
+export const emptyInbox = 'https://miro.medium.com/max/1088/1*DhOnyEHovQZ31rH00-VUDw.png';
+export const emptyProfilePic = 'https://ssl.gstatic.com/ui/v1/icons/mail/profile_mask2.png';
+
+export const VIEWS = {
+    inbox: 'inbox',
+    starred: 'starred',
+    sent: 'sent',
+    drafts: 'drafts'
+}
+
+export const EMPTY_TABS = {
+    inbox: {
+        heading: 'Your inbox is empty',
+        subHeading: "Mails that don't appear in other tabs will be shown here."
+    },
+    drafts: {
+        heading: "You don't have any saved drafts.",
+        subHeading: "Saving a draft allows you to keep a message you aren't ready to send yet."
+    },
+    starred: {
+        heading: 'No starred messages',
+        subHeading: "Stars let you give messages a special status to make them easier to find. To star a message, click on the star outline beside any message or conversation."
+    },
+    sent: {
+        heading: 'No sent messages!',
+        subHeading: 'Send one now!'
+    },
+    bin: {
+        heading: 'No conversations in Bin.',
+        subHeading: ''
+    }
+}
